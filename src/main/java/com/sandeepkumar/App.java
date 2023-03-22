6e43347dfbca2a6377d28a74d5e0e71441065520
sandeepdevopscloud <sandeepk08814@gmail.com>
2023-03-22 20:36:16 +0530

    Mavenjarv1

---
+++ src/main/java/com/sandeepkumar/App.java
@@ -0,0 +1,13 @@
+package com.sandeepkumar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
